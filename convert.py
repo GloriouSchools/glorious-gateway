1a8da75ce7c4c068f8fd5bbba4c21b699173238d
Fresh-Teacher <213285898+GloriouSchools@users.noreply.github.com>
2025-12-25 09:31:17 +0200

    update gallery

---
+++ convert.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+folder_path = r"C:\Users\Fresh Teacher\Pictures\Sent"
+
+all_files = os.listdir(folder_path)
+image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff')
+image_files = [f for f in all_files if f.lower().endswith(image_extensions)]
+
+data = {"images": image_files}
+
+with open("images.json", "w") as json_file:
+    json.dump(data, json_file, indent=4)
+
+print(f"JSON file created with {len(image_files)} images.")
